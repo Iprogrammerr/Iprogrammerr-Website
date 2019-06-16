5d07154ca310e29254a80a0c95026c97e4130d55
igor <ceigor94@gmail.com>
2019-06-16 07:53:04 +0200

    Remembering scroll position

--- src/main/resources/public/index.js
+++ src/main/resources/public/index.js
@@ -1,7 +1,12 @@
 const HIDDEN_CLASS = "hidden";
 const VISIBLE_CLASS = "visible";
 const ID_ATTRIBUTE = "data-id";
+const SCROLL_POS = "scroll-pos";
 
+let scrollPos = sessionStorage.getItem(SCROLL_POS);
+if (scrollPos == null) {
+    scrollPos = 0;
+}
 const skills = document.getElementsByClassName("skills")[0];
 const tags = skills.querySelectorAll("h2");
 const lists = skills.querySelectorAll("ul");
@@ -22,7 +27,7 @@ function toggleVisibility(element) {
 
 const experience = document.getElementsByClassName("experience")[0].children;
 
-for (let i = 0; i < experience.length; i+=2) {
+for (let i = 0; i < experience.length; i += 2) {
     let url = experienceUrl(experience[i].getAttribute(ID_ATTRIBUTE));
     experience[i].onclick = () => location.href = url;
     experience[i + 1].onclick = () => location.href = url;
@@ -31,3 +36,14 @@ for (let i = 0; i < experience.length; i+=2) {
 function experienceUrl(id) {
     return `experience/${id}`;
 }
+
+if (scrollPos > 0) {
+    window.scrollTo(0, scrollPos);
+}
+
+window.addEventListener("click", () => {
+    if (scrollPos != window.pageYOffset) {
+        scrollPos = window.pageYOffset;
+        sessionStorage.setItem(SCROLL_POS, scrollPos);
+    } 
+});
