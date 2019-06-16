d24e9bb142cc0b3e2dba95e11e73fd7d8294060e
igor <ceigor94@gmail.com>
2019-06-16 08:21:35 +0200

    Improving scroll saving solution

--- src/main/resources/public/index.js
+++ src/main/resources/public/index.js
@@ -3,10 +3,6 @@ const VISIBLE_CLASS = "visible";
 const ID_ATTRIBUTE = "data-id";
 const SCROLL_POS = "scroll-pos";
 
-let scrollPos = sessionStorage.getItem(SCROLL_POS);
-if (scrollPos == null) {
-    scrollPos = 0;
-}
 const skills = document.getElementsByClassName("skills")[0];
 const tags = skills.querySelectorAll("h2");
 const lists = skills.querySelectorAll("ul");
@@ -15,7 +11,26 @@ for (let i = 0; i < tags.length - 1; i++) {
     tags[i].onclick = () => toggleVisibility(lists[i]);
 }
 
-tags[tags.length - 1].onclick = () => location.href = "skills";
+tags[tags.length - 1].onclick = () => saveScrollAndExecute(() => location.href = "skills");
+
+const experience = document.getElementsByClassName("experience")[0].children;
+
+for (let i = 0; i < experience.length; i += 2) {
+    let url = experienceUrl(experience[i].getAttribute(ID_ATTRIBUTE));
+    experience[i].onclick = () => saveScrollAndExecute(() => location.href = url);
+    experience[i + 1].onclick = () => saveScrollAndExecute(() => location.href = url);
+}
+
+const links = document.querySelectorAll("a");
+links.forEach(e => sessionStorage.setItem(SCROLL_POS, window.pageYOffset));
+
+let scrollPos = sessionStorage.getItem(SCROLL_POS);
+if (scrollPos == null) {
+    scrollPos = 0;
+}
+if (scrollPos > 0) {
+    window.scrollTo(0, scrollPos);
+}
 
 function toggleVisibility(element) {
     if (element.className == HIDDEN_CLASS) {
@@ -23,27 +38,13 @@ function toggleVisibility(element) {
     } else {
         element.className = HIDDEN_CLASS;
     }
-}
+};
 
-const experience = document.getElementsByClassName("experience")[0].children;
-
-for (let i = 0; i < experience.length; i += 2) {
-    let url = experienceUrl(experience[i].getAttribute(ID_ATTRIBUTE));
-    experience[i].onclick = () => location.href = url;
-    experience[i + 1].onclick = () => location.href = url;
-}
+function saveScrollAndExecute(func) {
+    sessionStorage.setItem(SCROLL_POS, window.pageYOffset);
+    func();
+};
 
 function experienceUrl(id) {
     return `experience/${id}`;
-}
-
-if (scrollPos > 0) {
-    window.scrollTo(0, scrollPos);
-}
-
-window.addEventListener("click", () => {
-    if (scrollPos != window.pageYOffset) {
-        scrollPos = window.pageYOffset;
-        sessionStorage.setItem(SCROLL_POS, scrollPos);
-    } 
-});
+};
