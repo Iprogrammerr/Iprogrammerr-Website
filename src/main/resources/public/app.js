5eec2eb586b30a4ca283dd33f762a6b1f4d16bab
igor <ceigor94@gmail.com>
2019-06-22 12:53:49 +0200

    Removing hovers from mobile links

--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -10,11 +10,10 @@ const removeHover = () => {
     for (let h of hovers) {
         h.classList.replace(HOVER_CLASS, NO_HOVER);
     }
-    let links = document.querySelectorAll("a");
-    for (let a of links) {
+    for (let a of document.querySelectorAll("a")) {
         a.className = NO_HOVER;
     }
     window.removeEventListener(TOUCH_EVENT, removeHover);
-};
+};   
 
 window.addEventListener(TOUCH_EVENT, removeHover);
