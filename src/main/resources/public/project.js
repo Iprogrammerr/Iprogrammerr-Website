27c0abe2723038067ae77908a1d001dac47237a2
igor <ceigor94@gmail.com>
2019-06-30 09:57:13 +0200

    Changing project gallery (WIP)

--- src/main/resources/public/project.js
+++ src/main/resources/public/project.js
@@ -1,8 +1,10 @@
 import { IdNavigation } from "./id-navigation.js";
 
-const IMAGE_SMALL = "small";
-const IMAGE_MEDIUM = "medium";
-const IMAGE_LARGE = "large";
+const FOCUSED_IMAGE = "focused-image";
+const FOCUSED_IMAGE_HIDDEN = `${FOCUSED_IMAGE}-hidden`;
+const HIDDEN_SCROLL = "hidden";
+const HIDDEN_CLASS = "hidden";
+const IMAGE_URL_ATTRIBUTE = "data-image";
 
 const GALLERY = "gallery";
 const GALLERY_THREE = "gallery-three";
@@ -11,8 +13,8 @@ const GALLERY_ONE = "gallery-one";
 
 new IdNavigation("project").setup();
 
-let gallery = document.getElementById(GALLERY);
-let images = gallery.children;
+const gallery = document.getElementById(GALLERY);
+const images = gallery.children;
 if (images.length < 2) {
     gallery.classList.replace(GALLERY, GALLERY_ONE);
 } else if (images.length < 3) {
@@ -20,22 +22,50 @@ if (images.length < 2) {
 } else if (images.length < 4) {
     gallery.classList.replace(GALLERY, GALLERY_THREE);
 }
-let active = null;
-for (let i of images) {
-    i.onclick = (e) => {
-        if (active != e.target) {
-            active = e.target;
-            active.className = IMAGE_LARGE;
-            for (let i of images) {
-                if (i != active) {
-                    i.className = IMAGE_SMALL;
-                }
-            }
-        } else if (active == e.target) {
-            for (let i of images) {
-                i.className = IMAGE_MEDIUM;
-            }
-            active = null;
+
+const focusedImageContainer = document.getElementById("focused");
+const rootScroll = document.documentElement.style.overflow;
+focusedImageContainer.onclick = () => {
+    focusedImageContainer.className = FOCUSED_IMAGE_HIDDEN;
+    document.documentElement.style.overflow = rootScroll;
+};
+
+const focusedImage = focusedImageContainer.querySelector("img");
+
+let focusedIdx = -1;
+for (let i = 0; i < images.length; i++) {
+    images[i].onclick = (e) => {
+        focusedImage.src = e.target.getAttribute(IMAGE_URL_ATTRIBUTE);
+        document.documentElement.style.overflow = HIDDEN_SCROLL;
+        if (focusedImageContainer.className == FOCUSED_IMAGE_HIDDEN) {
+            focusedImageContainer.className = FOCUSED_IMAGE;
         }
+        focusedIdx = i;
+        setGalleryArrows();
+
     };
 }
+
+const previousImage = document.getElementById("previousImage");
+const nextImage = document.getElementById("nextImage");
+function setGalleryArrows() {
+    if (focusedIdx > 0) {
+        previousImage.classList.remove(HIDDEN_CLASS);
+    } else {
+        previousImage.classList.add(HIDDEN_CLASS);
+    }
+    if (focusedIdx < images.length - 1) {
+        nextImage.classList.remove(HIDDEN_CLASS);
+    } else {
+        nextImage.classList.add(HIDDEN_CLASS);
+    }
+};
+
+previousImage.onclick = e => {
+    images[focusedIdx - 1].click();
+    e.stopPropagation();
+};
+nextImage.onclick = e => {
+    images[focusedIdx + 1].click();
+    e.stopPropagation();
+};
