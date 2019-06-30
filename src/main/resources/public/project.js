073d0e2955e688878d48a972aa70a67a7d9ef81b
igor <ceigor94@gmail.com>
2019-06-30 11:57:30 +0200

    css - simplifying gallery classes

--- src/main/resources/public/project.js
+++ src/main/resources/public/project.js
@@ -6,22 +6,7 @@ const HIDDEN_SCROLL = "hidden";
 const HIDDEN_CLASS = "hidden";
 const IMAGE_URL_ATTRIBUTE = "data-image";
 
-const GALLERY = "gallery";
-const GALLERY_THREE = "gallery-three";
-const GALLERY_TWO = "gallery-two";
-const GALLERY_ONE = "gallery-one";
-
 new IdNavigation("project").setup();
-
-const gallery = document.getElementById(GALLERY);
-const images = gallery.children;
-if (images.length < 2) {
-    gallery.classList.replace(GALLERY, GALLERY_ONE);
-} else if (images.length < 3) {
-    gallery.classList.replace(GALLERY, GALLERY_TWO);
-} else if (images.length < 4) {
-    gallery.classList.replace(GALLERY, GALLERY_THREE);
-}
 
 const focusedImageContainer = document.getElementById("focused");
 const rootScroll = document.documentElement.style.overflow;
@@ -31,7 +16,7 @@ focusedImageContainer.onclick = () => {
 };
 
 const focusedImage = focusedImageContainer.querySelector("img");
-
+const images = document.getElementsByClassName("gallery")[0].children;
 let focusedIdx = -1;
 for (let i = 0; i < images.length; i++) {
     images[i].onclick = (e) => {
